6a0a2bf70f6fc4f9c6f84878fbd92b712d472ce3
34011-82-01 <34011-82-01@exchange.ad.afpanet>
2016-08-23 14:15:14 +0200

    546 en cours

--- src/main/FenetrePrincipale.java
+++ src/main/FenetrePrincipale.java
@@ -3,60 +3,80 @@
  */
 package main;
 
-import java.awt.Color;
-import java.awt.GraphicsConfiguration;
+import java.awt.Component;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  * Définit la fenêtre principale de l'application
- * @author david
+ * @author audric
  *
  */
-public class FenetrePrincipale extends JFrame 
+public class FenetrePrincipale extends JFrame implements ActionListener
 {
-
+	
+	JLabel jl1 = new JLabel("Nom");
+	JLabel jl2 = new JLabel("Prénom");
+	JTextField nom = new JTextField();
+	JTextField prenom = new JTextField();
+	JButton valider = new JButton("Valider");
+	
 	/**
 	 * @throws HeadlessException
 	 */
 	public FenetrePrincipale() throws HeadlessException 
 	{
 		this.setTitle("Recrutement"); 
-		this.setSize(400, 100);
+		this.setSize(400, 300);
 		this.setLocationRelativeTo(null);
 		
+		//Exemple de formulaire
 		
-		this.setContentPane(new Panneau()); // imbrication de notre panel dans notre fenêtre
+		JPanel pan = new JPanel();
+		
+		pan.setLayout(null);
+		
+		jl1.setBounds(20, 20, 100, 30);
+		
+		jl2.setBounds(20, 50, 100, 30);
+		
+		nom.setBounds(120, 20, 200, 30);
+		
+		prenom.setBounds(120, 50, 200, 30);
+		
+		valider.setBounds(100, 100, 80, 30);
+		
+		valider.addActionListener(this);
+		
+		pan.add(jl1);
+		pan.add(nom);
+		pan.add(jl2);
+		pan.add(prenom);
+		pan.add(valider);
+		
+		this.setContentPane(pan); // imbrication de notre panel dans notre fenêtre
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);	
 	}
 
-	/**
-	 * @param gc
-	 */
-	public FenetrePrincipale(GraphicsConfiguration gc) {
-		super(gc);
-		// TODO Auto-generated constructor stub
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource()==valider){
+			System.out.println("Validation");
+			String recupNom = nom.getText();
+			String recupPrenom = prenom.getText();
+		}
 	}
-
-	/**
-	 * @param title
-	 * @throws HeadlessException
-	 */
-	public FenetrePrincipale(String title) throws HeadlessException {
-		super(title);
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param title
-	 * @param gc
-	 */
-	public FenetrePrincipale(String title, GraphicsConfiguration gc) {
-		super(title, gc);
-		// TODO Auto-generated constructor stub
-	}
-
+	
 }
