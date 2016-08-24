ab31ee5bc2c5ce8acd5e7dd4edd3c065b3fd5039
34011-82-03 <34011-82-03@exchange.ad.afpanet>
2016-08-24 15:33:12 +0200

    Mise à jour panelCandidat.java

--- src/main/panelCandidat.java
+++ src/main/panelCandidat.java
@@ -1,45 +1,7 @@
 package main;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-/**
- * 
- * 
- * @author Cyril
- * @version v1.00
- * 
- */
-
-public class panelCandidat extends JPanel  {
-	
-	protected JPanel panel1;
-	
-	protected JLabel iconeEntreprise;
-	private ImageIcon logo;
-
-	public panelCandidat() 
-	{
-		
-		panel1 = new JPanel(null);
-		
-		iconeEntreprise = new JLabel(new ImageIcon("logoAFPA.png"));		
-		panel1.setLayout(new FlowLayout());
-		
-	
-		
-		panel1.add(iconeEntreprise);	
-		this.add(panel1);
-		
-	}
-	
-	
-	
+public class panelCandidat extends JPanel {
 
 }
