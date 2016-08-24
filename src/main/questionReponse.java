c038cf71e6a810cead89602ddf67ed87ebe9a956
34011-82-02 <34011-82-02@exchange.ad.afpanet>
2016-08-24 08:39:25 +0200

    Ajout questionReponse.java

---
+++ src/main/questionReponse.java
@@ -0,0 +1,31 @@
+package main;
+
+public class questionReponse 
+{
+	/**
+	 * Structure utilisée pour générer le tableau contenant les questions auquelles le candidat devra répondre
+	 * 
+	 * categorieQuestion - La catégorie de la question qui peut être :
+	 * 							"CJ" pour Culture Java 
+	 * 							"CG" pour Culture Générale Informatique
+	 * 							"E" pour Langage Exotique
+	 * 							"S" pour Stress
+	 * 					
+	 * numeroQuestion - Le numéro que la question a dans le panel de questions
+	 * 
+	 * reponseCorrecte - La réponse correcte à la question
+	 * 
+	 * reponseCandidat - La réponse du candidat
+	 * 
+	 * timerAuMomentDeLaReponse - On récupère le timer au moment où le candidat répond à la question		
+	 */
+	
+	byte categorieQuestion;
+	byte numeroQuestion;
+	byte reponseCorrecte;
+	byte reponseCandidat;
+	int timerAuMomentDeLaReponse; // En secondes pour le moment
+	
+	
+	
+}
