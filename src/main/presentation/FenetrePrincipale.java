8ad1a61e8ec9526df40801b88ab1dd2eff233233
34011-82-01 <34011-82-01@exchange.ad.afpanet>
2016-09-06 15:29:17 +0200

    Menu Quiz

--- src/main/presentation/FenetrePrincipale.java
+++ src/main/presentation/FenetrePrincipale.java
@@ -35,6 +35,7 @@ import main.metier.Recruteur;
  * @author Cyril
  * @author Damien
  * @author Florent
+ * @author Audric
  * 
  * @version 1.02
  */
@@ -47,6 +48,9 @@ public class FenetrePrincipale extends JFrame implements ActionListener
 	private panelCandidat panCandidat;
 	private panelQuestion panQuestion;
 	private panelFin panFin;
+	private panelAjouterQuestion panAjouterQuestion;
+	private panelModifierQuestion panModifierQuestion;
+	private panelSupprimerQuestion panSupprimerQuestion;
 	private EnregistrementDonnee ed;
 	
 
@@ -377,6 +381,32 @@ public class FenetrePrincipale extends JFrame implements ActionListener
 			
 			this.getContentPane().removeAll();
 			this.setContentPane(panAccueil);
+			this.validate();
+		}
+		
+		
+		//Passage du panFormulaire aux panels ajout modif et supprimer question
+		
+		panAjouterQuestion = new panelAjouterQuestion();
+		panModifierQuestion = new panelModifierQuestion();
+		panSupprimerQuestion = new panelSupprimerQuestion();
+		
+		if(arg0.getSource() == this.panFormulaire.itemAjoutQuestion)
+		{
+			this.getContentPane().removeAll();
+			this.setContentPane(panAjouterQuestion);
+			this.validate();
+		}
+		if(arg0.getSource() == this.panFormulaire.itemModifierQuestion)
+		{
+			this.getContentPane().removeAll();
+			this.setContentPane(panModifierQuestion);
+			this.validate();
+		}
+		if(arg0.getSource() == this.panFormulaire.itemSupprimerQuestion)
+		{
+			this.getContentPane().removeAll();
+			this.setContentPane(panSupprimerQuestion);
 			this.validate();
 		}
 		
