6046f4d552a5b3ccdfb9aeadd85d11d8874204aa
34011-82-12 <34011-82-12@exchange.ad.afpanet>
2016-09-14 09:25:49 +0200

    rajout timerstress.java

---
+++ src/main/metier/TimerStress.java
@@ -0,0 +1,24 @@
+/**
+ * Il s'agit du timer du panel de la question 11 (question stress), le timer est initialisé à 2min.
+ */
+package main.metier;
+
+import main.metier.TimerGeneral;
+
+/**
+ * @author Benjamin
+ *version 1.0
+ */
+
+//Héritage de la class timer general
+public class TimerStress extends TimerGeneral {
+		
+	public TimerStress(int secondPassed){
+		super(secondPassed);
+		
+		//Timer stress 2min
+		TimerGeneral TimerS = new TimerGeneral(120);
+		System.out.println(TimerS);
+		
+	}
+}
